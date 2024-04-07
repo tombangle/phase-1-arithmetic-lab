fe4c6638e55bb5df4310b1c5ed01d1315765e87c
Tom Bangle <tombangle@gmail.com>
2024-04-07 09:44:40 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 let multiply;
-let num1 = 32;
+let num1 = 31;
 let num2 = 2;
 multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+
+let max = Math.max(10, 20, 15, 5);
